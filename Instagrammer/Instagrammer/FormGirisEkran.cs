8af0f2334136fa1dbf5bce7ca7a5c5b0fd353600
Omer Faruk Ozturk <152120171018@ogrenci.ogu.edu.tr>
2021-06-01 16:17:10 +0300

    Instagram bot realized within the scope of formal languages and automata assignment

---
+++ Instagrammer/Instagrammer/FormGirisEkran.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Instagrammer
+{
+    public partial class FormGirisEkran : Form
+    {
+        public FormGirisEkran()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtPassword.UseSystemPasswordChar = true;
+        }
+        
+        private void btnLogIn_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            FormTakipBilgiSistemi ftae = new FormTakipBilgiSistemi();
+            ftae.kullaniciAdi = txtUserName.Text;
+            ftae.parola = txtPassword.Text;
+            ftae.ShowDialog();
+            ftae.Show();
+            
+        }
+
+        private void btnLogOut_Click(object sender, EventArgs e)
+        {
+            DialogResult giriskapanis = MessageBox.Show("Programı kapatmak istediğinizden emin misiniz ? ", "Çıkış", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (giriskapanis == DialogResult.Yes)
+                Environment.Exit(0);
+        }
+
+        private void panelUserNamePassword_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void FormGirisEkran_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult giriskapanis = MessageBox.Show("Programı kapatmak istediğinizden emin misiniz ? ", "Çıkış", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (giriskapanis == DialogResult.No)
+            {
+                e.Cancel = true;
+                return;
+            }
+            Environment.Exit(0);
+        }
+
+        private void rTxtSiteLinki_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+
+            System.Diagnostics.Process.Start(e.LinkText);
+        }
+
+        private void txtUserName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
